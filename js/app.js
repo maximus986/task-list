d02e6f91107da43dea1a2fc1b7176e4a271acb32
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2019-01-07 11:55:12 +0100

    Create add task function

---
+++ js/app.js
@@ -0,0 +1,35 @@
+(function IIFE() {
+    const form = document.querySelector(".task-form");
+    const taskInput = document.querySelector(".task");
+    const tasksList = document.querySelector(".list-group");
+    const filterTasks = document.querySelector(".filter-tasks");
+    const clearTasksBtn = document.querySelector(".clear-tasks");
+
+    loadEventListeners();
+
+
+    function loadEventListeners() {
+        form.addEventListener("submit", addTask);
+    }
+
+    function addTask(e) {
+        if (taskInput.value === "") {
+            alert("Please add a task.");
+        } else {
+            const li = document.createElement("li");
+            li.className = "list-group-item d-flex justify-content-between align-items-center";
+            const liTask = document.createTextNode(taskInput.value);
+            li.appendChild(liTask);
+            const deleteTask = document.createElement("a");
+            deleteTask.className = "delete-task";
+            //deleteTask.setAttribute("href", "#");
+            deleteTask.innerHTML = "<i class='fa fa-trash-alt text-dark'></i>";
+            li.appendChild(deleteTask);
+            tasksList.appendChild(li);
+            console.log(li);
+            taskInput.value = "";
+        }
+        e.preventDefault();
+    }
+})();
+
